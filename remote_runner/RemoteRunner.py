68d02d5a6d5efde99af66ef461a52d5ffe429d1d
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-10-04 15:23:23 +0300

    ref: Adapt code for python3, make variable ssh/config, refactor connection procedure

--- remote_runner/RemoteRunner.py
+++ remote_runner/RemoteRunner.py
@@ -12,6 +12,8 @@ from .RemoteTask import RemoteTask
 
 
 class RemoteRunner(object):
+    ssh_config_file = "~/.ssh/config"
+    logger = logging.getLogger("remote_runner.RemoteRunner")
 
     def __init__(self, host, remote_tmp_root="~/remote_runner_tmp", remote_environment_setup=[]):
         self.host = host
@@ -27,29 +29,30 @@ class RemoteRunner(object):
         client._policy = paramiko.WarningPolicy()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+        client.connect(**self.get_host_config())
+        self.client = client
+
+    def get_host_config(self):
         ssh_config = paramiko.SSHConfig()
-        user_config_file = os.path.expanduser("~/.ssh/config")
+        user_config_file = os.path.expanduser(self.ssh_config_file)
+
         if os.path.exists(user_config_file):
             with open(user_config_file) as f:
                 ssh_config.parse(f)
 
         cfg = {'hostname': self.host}
-
         user_config = ssh_config.lookup(cfg['hostname'])
         if 'hostname' in user_config:
             cfg['hostname'] = user_config['hostname']
         if 'user' in user_config:
             cfg['username'] = user_config['user']
-
         for k in ('port',):
             if k in user_config:
                 cfg[k] = int(user_config[k])
-
         if 'proxycommand' in user_config:
             cfg['sock'] = paramiko.ProxyCommand(user_config['proxycommand'])
 
-        client.connect(**cfg)
-        self.client = client
+        return cfg
 
     def disconnect(self):
         self.client.close()
@@ -59,9 +62,9 @@ class RemoteRunner(object):
         :param (RemoteTask) task: task
         :return: None
         """
-        self.logger().info("RemoteRunner[%s]: stagein" % task.name)
+        self.logger.info("RemoteRunner[%s]: stagein" % task.name)
         if self.remote_tmp_root is None: return
-        self.logger().info("Preparing [%s] to run" % self.host)
+        self.logger.info("Preparing [%s] to run" % self.host)
 
         local_wd = os.path.abspath(task.wd)
         remote_wd = self.remote_tmp_wd(local_wd)
@@ -69,14 +72,14 @@ class RemoteRunner(object):
 
         ecode = subprocess.call(["rsync", "-av"]
                                 + ["--exclude=%s" % pattern for pattern in task.stagein_exclude_patterns]
-                                + [local_wd+"/"]
-                                + [self.host + ":" + remote_wd ])
+                                + [local_wd + "/"]
+                                + [self.host + ":" + remote_wd])
         assert (ecode == 0)
 
     def remote_tmp_wd(self, directory):
         if self.remote_tmp_root is None: return directory
         m = hashlib.md5()
-        m.update(os.path.abspath(directory))
+        m.update(os.path.abspath(directory).encode('utf-8'))
         remote_wd = self.remote_tmp_root + "/" + m.hexdigest()
         return remote_wd
 
@@ -97,11 +100,11 @@ class RemoteRunner(object):
                 self.client.close()
                 raise
         except KeyboardInterrupt as e:
-            self.logger().debug("RemoteRunner[%s]: keyboard interrupt" % task.name)
-            self.report_error(task, "iterrupted")
+            self.logger.debug("RemoteRunner[%s]: keyboard interrupt" % task.name)
+            self.report_error(task, "interrupted")
             raise
         except:
-            self.logger().debug("RemoteRunner[%s]: unknown error" % task.name)
+            self.logger.debug("RemoteRunner[%s]: unknown error" % task.name)
             self.report_error(task, "unknown error")
             raise
 
@@ -112,7 +115,7 @@ class RemoteRunner(object):
         :param rsync_period:
         :return:
         """
-        self.logger().info("RemoteRunner[%s]: remote run" % task.name)
+        self.logger.info("RemoteRunner[%s]: remote run" % task.name)
         import threading
 
         remote_run_complete = threading.Event()
@@ -122,9 +125,13 @@ class RemoteRunner(object):
             import time
             try:
                 cmd = "\n".join(
-                    self.remote_environment_setup + [
+                    [
                         "source /etc/profile",
-                        "source ~/.profile",
+                        "source ~/.profile"
+                    ] +
+                    self.remote_environment_setup
+                    +
+                    [
                         "cd %s" % self.remote_tmp_wd(task.wd),
                         "nohup python -c 'from remote_runner import RemoteTask;task=RemoteTask.load_state(\"{state}\",cd_to_wd=False); task.run();' > stdout 2> stderr &".format(
                             state=state),
@@ -132,15 +139,15 @@ class RemoteRunner(object):
                     ])
 
                 exit_code, out, err = self.exec_cmd(cmd)
-                remote_pid = int(out.strip().split("\n")[-1])
+                remote_pid = int(out.strip().split(b"\n")[-1])
 
-                self.logger().debug("RemoteRunner[%s]: remote pid %d" % (task.wd, remote_pid))
+                self.logger.debug("RemoteRunner[%s]: remote pid %d" % (task.wd, remote_pid))
 
                 kill_tries = 0
                 while True:
                     c, o, e = self.exec_cmd("ps -p %d -o comm=" % remote_pid, tee_to_host=False)
                     if c != 0:
-                        self.logger().debug("RemoteRunner[%s]: remote run complete " % task.wd)
+                        self.logger.debug("RemoteRunner[%s]: remote run complete " % task.wd)
                         remote_run_complete.set()
                         break
                     else:
@@ -148,21 +155,21 @@ class RemoteRunner(object):
                         # RemoteRunner.log_debug("RemoteRunner[%s]: remote ps:" % md._name + o)
 
                     if self.exiting.is_set():
-                        self.logger().debug("RemoteRunner[%s]: exiting is set " % task.name)
+                        self.logger.debug("RemoteRunner[%s]: exiting is set " % task.name)
                         self.thread_exception = KeyboardInterrupt("Interrupted")
                         c, o, e = self.exec_cmd("kill -15 %d" % (remote_pid))
-                        self.logger().debug("RemoteRunner[%s]: killing -15 remote job: %s, %s" % (task.name, o, e))
+                        self.logger.debug("RemoteRunner[%s]: killing -15 remote job: %s, %s" % (task.name, o, e))
                         kill_tries += 1
                         if kill_tries > 3:
                             c, o, e = self.exec_cmd("kill -15 %d" % (remote_pid))
-                            self.logger().debug(
+                            self.logger.debug(
                                 "RemoteRunner[%s]: killing -9 remote job: %s, %s" % (task.name, o, e))
 
                     time.sleep(3.0)
 
                 if exit_code != 0:
-                    self.logger().debug("RemoteRunner[%s]: remote exit code %d" % (task.name, exit_code))
-                    self.report_error(task, "run error", body="Output:\n%s\n\nStderr:\n%s\n\n"%(out,err))
+                    self.logger.debug("RemoteRunner[%s]: remote exit code %d" % (task.name, exit_code))
+                    self.report_error(task, "run error", body="Output:\n%s\n\nStderr:\n%s\n\n" % (out, err))
                     self.thread_exception = Exception("Return status is %d" % exit_code)
             except Exception as e:
                 print(e)
@@ -175,25 +182,25 @@ class RemoteRunner(object):
         t = threading.Thread(target=call_run)
         t.start()
 
-        self.logger().debug("RemoteRunner[%s]: thread spawned" % (task.name))
+        self.logger.debug("RemoteRunner[%s]: thread spawned" % (task.name))
 
         if self.remote_tmp_root is not None:
             while not remote_run_complete.is_set() and not self.exiting.is_set():
                 remote_run_complete.wait(timeout=rsync_period)
                 self._rsync_trj_home_with_remote(task)
 
-        self.logger().debug("RemoteRunner[%s]: joining thread" % (task.name))
+        self.logger.debug("RemoteRunner[%s]: joining thread" % (task.name))
 
         join_thread(t)
 
-        self.logger().debug("RemoteRunner[%s]: thread joined" % (task.name))
+        self.logger.debug("RemoteRunner[%s]: thread joined" % (task.name))
 
     def stageout(self, task):
         """
         :param (RemoteTask) task:
         :return: None
         """
-        self.logger().debug("RemoteRunner[%s]: stageout" % task.name)
+        self.logger.debug("RemoteRunner[%s]: stageout" % task.name)
         if self.remote_tmp_root is None: return
         self._rsync_trj_home_with_remote(task)
         self.exec_cmd("rm -r %s" % self.remote_tmp_wd(task.wd))
@@ -210,10 +217,10 @@ class RemoteRunner(object):
         exclude_list = task.stageout_exclude_patterns
 
         rsync_cmd = ["rsync", "-a"] + exclude_list + [self.host + ":" + remote_trj_home + "/", trj_home + "/"]
-        self.logger().debug("RemoteRunner[%s]: rsync command: %s" % (task.name, rsync_cmd))
+        self.logger.debug("RemoteRunner[%s]: rsync command: %s" % (task.name, rsync_cmd))
 
         ecode = subprocess.call(rsync_cmd)
-        self.logger().debug("RemoteRunner[%s]: rsync exit code: %d" % (task.name, ecode))
+        self.logger.debug("RemoteRunner[%s]: rsync exit code: %d" % (task.name, ecode))
 
         if ecode != 0:
             self.rsync_fails_in_row += 1
@@ -224,35 +231,35 @@ class RemoteRunner(object):
 
     def exec_cmd(self, cmd, stdin=None, tee_to_host=True):
         import time
-        chan = self.client.get_transport().open_session()
-        chan.exec_command(cmd)
+        channel = self.client.get_transport().open_session()
+        channel.exec_command(cmd)
         if stdin is not None:
-            inp = chan.makefile('wb', -1)
+            inp = channel.makefile('wb', -1)
             inp.write(stdin)
             inp.flush()
             inp.close()
-            chan.shutdown_write()
+            channel.shutdown_write()
 
-        out = ''
-        err = ''
+        out = b''
+        err = b''
 
-        while not chan.exit_status_ready():  # monitoring process
-            while chan.recv_ready():
-                out += chan.recv(80)
-            while chan.recv_stderr_ready():
-                err += chan.recv_stderr(80)
+        while not channel.exit_status_ready():  # monitoring process
+            while channel.recv_ready():
+                out += channel.recv(80)
+            while channel.recv_stderr_ready():
+                err += channel.recv_stderr(80)
             time.sleep(1)
 
-        while chan.recv_ready():
-            out += chan.recv(80)
-        while chan.recv_stderr_ready():
-            err += chan.recv_stderr(80)
+        while channel.recv_ready():
+            out += channel.recv(80)
+        while channel.recv_stderr_ready():
+            err += channel.recv_stderr(80)
 
         if tee_to_host:
-            sys.stderr.write(err)
-            sys.stdout.write(out)
+            sys.stderr.write(err.decode('utf-8'))
+            sys.stdout.write(out.decode('utf-8'))
 
-        return chan.recv_exit_status(), out, err
+        return channel.recv_exit_status(), out, err
 
     def report_error(self, task, message, body=""):
         """
@@ -274,14 +281,6 @@ class RemoteRunner(object):
             postman.send_mail(subject, text)
         except:
             print("Can't send mail", file=sys.stderr)
-
-    @staticmethod
-    def logger():
-        """
-        :return: logger
-        :rtype: logging.Logger
-        """
-        return logging.getLogger("remote_runner.RemoteRunner")
 
 
 #
