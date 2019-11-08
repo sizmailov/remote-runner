462bde3b4869994174521c16d28a2cf66935f2e7
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-11-08 12:18:33 +0300

    test: add test for SyncSSHWorker

---
+++ tests/run_ssh_with_sync.py
@@ -0,0 +1,86 @@
+import random
+from remote_runner import *
+from remote_runner.utility import ChangeToTemporaryDirectory
+from ssh_common import sync_ssh_worker_factory
+
+
+class MyTask(Task):
+
+    def __init__(self, name, data):
+        if not os.path.exists(name):
+            os.mkdir(name)
+        Task.__init__(self, wd=Path(name).absolute())
+        self.data = data
+        self.save(Path(name, self.state_filename))
+
+    def run(self):
+        import time
+        with open("output", "w", 1) as fout:
+            for x in self.data:
+                fout.write(f"{x}")
+                fout.flush()
+                time.sleep(3.0 / len(self.data))
+
+
+def read_file_content(filename):
+    try:
+        with open(filename) as f:
+            return f.read()
+    except OSError as e:
+        return ""
+
+
+class KeepReading(threading.Thread):
+
+    def __init__(self, filename: str, period: float):
+        super().__init__()
+        self.stop_reading = threading.Event()
+        self.period = period
+        self.data = []
+        self.filename = filename
+
+    def run(self):
+        self.stop_reading.clear()
+        while not self.stop_reading.is_set():
+            self.data.append(read_file_content(self.filename))
+            self.stop_reading.wait(self.period)
+        self.data.append(read_file_content(self.filename))
+
+
+with ChangeToTemporaryDirectory():
+    N = 1000
+    random_data = [random.randint(0, 100) for i in range(N)]
+
+    task_name = "syncing_task"
+    reader = KeepReading(Path(task_name, "output"), period=0.4)
+
+    reader.start()
+    worker = sync_ssh_worker_factory()
+
+    Pool([
+        worker,
+    ]).run([
+        MyTask(name=task_name, data=random_data)
+    ])
+
+    reader.stop_reading.set()
+    reader.join()
+
+    prev = reader.data[0]
+    n_diffs = 0
+    for curr in reader.data[1:]:
+        assert curr.startswith(prev)
+        if curr != prev:
+            n_diffs += 1
+        prev = curr
+
+    assert n_diffs > 3  # output was copied multiple times
+
+    expected_text = "".join(str(x) for x in random_data)
+    assert expected_text == reader.data[-1]
+
+    # remote root is cleaned
+    tmp_remote_dirs = list(
+        d for d in Path(worker.remote_root.expanduser()).glob("*") if d.is_dir()
+    )
+    assert tmp_remote_dirs == []
--- tests/ssh_common.py
+++ tests/ssh_common.py
@@ -1,10 +1,18 @@
 import os
 import sys
-from remote_runner import SSHWorker
+from remote_runner import SSHWorker, SyncSSHWorker
 
 
 def ssh_worker_factory():
     worker = SSHWorker(host='localhost')
+    worker.remote_user_rc = f"""
+source {os.path.abspath(os.path.join(os.path.dirname(sys.executable), "activate"))}
+"""
+    return worker
+
+
+def sync_ssh_worker_factory():
+    worker = SyncSSHWorker(sync_period=0.2, host='localhost')
     worker.remote_user_rc = f"""
 source {os.path.abspath(os.path.join(os.path.dirname(sys.executable), "activate"))}
 """
