4118d046aed1adf6572d584242d8676cf37dd28d
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-11-27 20:14:30 +0300

    Reset all signal handlers once exception is raised

--- remote_runner/errors.py
+++ remote_runner/errors.py
@@ -22,7 +22,8 @@ class RaiseOnSignals:
             raise StopCalculationError()
         finally:
             # protect from double kill (e.g. accident too many Ctrl-C hits)
-            signal.signal(signum, signal.SIG_IGN)
+            signal.signal(signal.SIGTERM, signal.SIG_IGN)
+            signal.signal(signal.SIGINT, signal.SIG_IGN)
 
     @staticmethod
     def _raise_on_signals(signums=(signal.SIGTERM, signal.SIGINT)):
